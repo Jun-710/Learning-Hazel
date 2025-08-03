b697ee1e2a1d5bf1dcb4b3cff2db79dd9d8f0fbf
Jun-710 <l13126938086@163.com>
2025-08-03 22:00:21 +0800

    1

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -1,6 +1,6 @@
 #pragma once
 //用于头文件的包含
 #include"Hazel/Application.h"
-
+#include"Hazel/Log.h"
 //进入接口
 #include"Hazel/EntryPoint.h"
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -2,6 +2,9 @@
 #ifdef HZ_PLATFORM_WINDOWS
 extern Hazel::Application* Hazel::CreateApplication();
 int main(int argc ,char **argv) {
+	Hazel::Log::init();
+	HZ_CORE_WARN("ok core");
+	HZ_INFO("ok client");
 	auto app = Hazel::CreateApplication();
 	app->run();
 	delete app;
